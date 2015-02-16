f6bece0852e0eba51983711e8a732a31b786c394
Erik Griffin <griffin.erik@gmail.com>
2015-02-16 16:34:59 +0000

    Experimenting. Menu IS AN Array.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,21 +1,17 @@
-class Menu
+class Menu < Array
 
-
-  def initialize
-    @contents = []
-  end
 
   def add_dish(dish)
-    @contents << dish
+    self << dish
   end
 
   def get_dishes
-    @contents
+    self
   end
 
   def get_menu
     menu = ""
-    @contents.each do |dish|
+    each do |dish|
       menu = menu + menu_list_item(dish)
     end
     menu
@@ -27,11 +23,11 @@ class Menu
   private
 
   def longest_dish_name
-    @contents.inject {|max, word| max.name.length > word.name.length ? max : word}.name.length
+    inject {|max, word| max.name.length > word.name.length ? max : word}.name.length
   end
 
   def longest_dish_price
-    get_price(@contents.inject {|max, price| (get_price(max)).length > (get_price(price)).length ? max : price}).length
+    get_price(inject {|max, price| (get_price(max)).length > (get_price(price)).length ? max : price}).length
   end
 
   def get_price(dish)
